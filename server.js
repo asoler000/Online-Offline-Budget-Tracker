f85aef47bb817303c7a0053a7687e2d31c6dbc24
Adriana Monster <AdrianaMonster@profretail.local>
2020-12-10 13:36:14 -0500

    deploying again had errors

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dbUser:<Daniel@1>@cluster0.mzt48.mongodb.net/<budget>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dbUser:Daniel@1@cluster0.mzt48.mongodb.net/budget?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("budget").collection("budget-tracker");
   // perform actions on the collection object
   client.close();
 });
